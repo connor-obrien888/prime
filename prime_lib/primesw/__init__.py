2dea1079db28f6b8cd1e4684e0636e60135bc39d
Connor O'Brien <obrienco@bu.edu>
2025-02-20 15:27:35 -0500

    Scaler and model import fix, namespace change
    Reworked the way that scalers and model resources are loaded so that it works regardless of install location and so that users need not keep track of model hyperparameters. Also added statment to __init__.py importing the prime class so that users don't have to type primesw.prime.prime() UGH GROSS EW

---
+++ prime_lib/primesw/__init__.py
@@ -0,0 +1,7 @@
+"""
+Python package `primesw` is an implementation of the Probabilistic Regressor for Input to the Magnetosphere Estimation (PRIME) L1-to-Earth solar wind propagation algorithm.
+For details on the algorithm development, see the [publication](https://www.frontiersin.org/articles/10.3389/fspas.2023.1250779/full).
+`primesw` mainly consists of the `primesw.prime` class,
+"""
+
+from .prime import prime
